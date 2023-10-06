eecac29ac72a4a3ccbe59e183e0a868fe244bb2b
hansolpn <pinecone171@gmail.com>
2023-10-06 17:51:12 +0900

    231006 spring spring-jdbc mybatis

--- SpringWebBasic/src/main/java/com/spring/basic/score/service/ScoreService.java
+++ SpringWebBasic/src/main/java/com/spring/basic/score/service/ScoreService.java
@@ -3,29 +3,27 @@ package com.spring.basic.score.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.spring.basic.score.dto.ScoreListResponseDTO;
 import com.spring.basic.score.dto.ScoreRequestDTO;
 import com.spring.basic.score.entity.Score;
-import com.spring.basic.score.repository.IScoreRepository;
+import com.spring.basic.score.repository.IScoreMapper;
 
-//import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 // 컨트롤러와 레파지토리 사이에 배치되어 기타 비즈니스 로직 처리
 // ex) 값을 가공, 예외 처리, dto로 변환, 트랜잭션 등등...
 @Service // 빈 등록
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class ScoreService {
 
-	private final IScoreRepository scoreRepository;
+	private final IScoreMapper scoreRepository;
 
-	@Autowired
-	public ScoreService(@Qualifier("jdbc") IScoreRepository scoreRepository) {
-		this.scoreRepository = scoreRepository;
-	}
+//	@Autowired
+//	public ScoreService(@Qualifier("spring") IScoreRepository scoreRepository) {
+//		this.scoreRepository = scoreRepository;
+//	}
 	
 	// 등록 중간 처리
 	// 컨트롤러는 나에게 DTO를 줌
@@ -74,6 +72,6 @@ public class ScoreService {
 		
 		score.changeScore(dto);
 		
-		scoreRepository.updateByStuNum(stuNum, score);
+		scoreRepository.modify(score);
 	}
 }
