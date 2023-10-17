4cdbed49214575fa2475436a651c986cab6859ae
hansolpn <pinecone171@gmail.com>
2023-10-17 17:30:10 +0900

    231017 spring

--- MyWeb/src/main/java/com/spring/myweb/user/service/UserService.java
+++ MyWeb/src/main/java/com/spring/myweb/user/service/UserService.java
@@ -2,6 +2,8 @@ package com.spring.myweb.user.service;
 
 import org.springframework.stereotype.Service;
 
+import com.spring.myweb.user.dto.UserJoinRequestDTO;
+import com.spring.myweb.user.entity.User;
 import com.spring.myweb.user.mapper.IUserMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,29 @@ public class UserService {
 
 	public int idCheck(String id) {
 		return mapper.idCheck(id);
+
+	}
+
+	public void join(UserJoinRequestDTO dto) {
+		mapper.join(User.builder()
+						.userId(dto.getUserId())
+						.userPw(dto.getUserPw())
+						.userName(dto.getUserName())
+						.userPhone1(dto.getUserPhone1())
+						.userPhone2(dto.getUserPhone2())
+						.userEmail1(dto.getUserEmail1())
+						.userEmail2(dto.getUserEmail2())
+						.addrBasic(dto.getAddrBasic())
+						.addrDetail(dto.getAddrDetail())
+						.addrZipNum(dto.getAddrZipNum())
+						.build());
+
+	}
+
+	public String login(String userId) {
+		return mapper.login(userId);
 		
 	}
-	
-	
+
+
 }
--- MyWeb/src/main/webapp/WEB-INF/views/include/header.jsp
+++ MyWeb/src/main/webapp/WEB-INF/views/include/header.jsp
@@ -36,7 +36,7 @@
 
                         <div class="collapse navbar-collapse" id="myNavbar">
                             <ul class="nav navbar-nav">
-                                <li class="active"><a href="<c:url value='/' />">Main</a></li>
+                                <li class="active"><a href="/myweb/">Main</a></li>
                                 <li><a href="/myweb/freeboard/freeList">자유게시판</a></li>
                                 <!-- 
 				<li><a href="#">강의</a></li>
